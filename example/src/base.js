a9e9664e0f5ebe81d5571679fce2901559299164
lujun <lujun@ali-6c96cfd98a9b.local>
2017-12-20 19:38:01 +0800

    “”

--- example/src/base.js
+++ example/src/base.js
@@ -3,7 +3,6 @@ import React from 'react'
 import {render} from 'react-dom'
 import PullRefresh from '../pull-refresh/index'
 
-//https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=%E4%B8%AD%E5%9B%BD
 
 class App extends React.Component{
   render(){
@@ -12,7 +11,7 @@ class App extends React.Component{
         <PullRefresh />
         {
         	Object.keys(window).map((item)=>{
-        		return <div className="box">{item}</div>
+        		return <div className="box" key={item}>{item}</div>
         	})
         }
       </div>
